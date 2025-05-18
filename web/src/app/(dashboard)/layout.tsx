ce9d1ca13637ef60a2191dc68196949058fd4628
josuefuentesdev <josuefuentesdev@gmail.com>
2025-05-18 12:03:35 +0200

    feat: avoid scroll when sidebar is expanded as suggested in https://github.com/shadcn-ui/ui/issues/5545#issuecomment-2867696778

--- web/src/app/(dashboard)/layout.tsx
+++ web/src/app/(dashboard)/layout.tsx
@@ -19,14 +19,14 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="min-w-0">
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
           <BreadcrumbNav />
           <div className="flex-1" />
           <ModeToggle />
-          <UserAccountNav user={session?.user} />
+          <UserAccountNav user={session.user} />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
