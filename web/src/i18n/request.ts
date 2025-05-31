60dde3989069c49a9b2208a8ac17f92cc2ed4c26
josuefuentesdev <josuefuentesdev@gmail.com>
2025-05-31 22:42:34 +0200

    feat: add next intl global format "fullShort"

--- web/src/i18n/request.ts
+++ web/src/i18n/request.ts
@@ -7,6 +7,18 @@ export default getRequestConfig(async () => {
   return {
     locale,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../../messages/${locale}.json`)).default,
+    formats: {
+      dateTime: {
+        fullShort: {
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric'
+        }
+      },
+    }
   };
 });
